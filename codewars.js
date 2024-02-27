d64dc049020f7455687988d4ac9843477f9757f7
Tomaster134 <tomaster134@gmail.com>
2024-02-27 14:30:08 -0500

    final commit

--- codewars.js
+++ codewars.js
@@ -19,4 +19,8 @@ function humanReadable(seconds) {
   return `${hour}:${minute}:${second}`;
 }
 
-// 2.
+// 2. Create Phone Number
+
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
